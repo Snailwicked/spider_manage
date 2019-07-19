ddfe1e8e9d514586ca93d78ed67f810ff92b159f
bsnail <2575652913@qq.com>
2019-07-19 16:53:36 +0800

    更新任务队列

--- tasks/workers.py
+++ tasks/workers.py
@@ -1,18 +1,11 @@
-# coding:utf-8
+# # coding:utf-8
 import os
-from datetime import timedelta
-
+#
 from celery import Celery, platforms
-from kombu import Exchange, Queue
-
 from config.conf import get_broker_and_backend
-# allow celery worker started by root
 platforms.C_FORCE_ROOT = True
-
-worker_log_path = os.path.join(os.path.dirname(os.path.dirname(__file__)) + './logs', 'celery.log')
-beat_log_path = os.path.join(os.path.dirname(os.path.dirname(__file__)) + './logs', 'beat.log')
 broker, backend = get_broker_and_backend()
-
+#
 tasks = ['tasks.xpath']
 
 app = Celery('spider_task', include=tasks, broker=broker, backend=backend)
@@ -20,20 +13,29 @@ app = Celery('spider_task', include=tasks, broker=broker, backend=backend)
 app.conf.update(
     CELERY_TIMEZONE='Asia/Shanghai',
     CELERY_ENABLE_UTC=True,
-    CELERYD_LOG_FILE=worker_log_path,
-    CELERYBEAT_LOG_FILE=beat_log_path,
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json',
-    CELERYBEAT_SCHEDULE={
-        'xpath_task': {
-            'task': 'tasks.xpath.excute_xpath_task',
-            'schedule': timedelta(hours=1),
-            'options': {'queue': 'xpath_queue', 'routing_key': 'for_xpath'}
-        },
+    CELERY_ROUTES={
+        'tasks.xpath.excute_xpath_task':
+            {
+                'queue': 'xpath_queue',
+                'routing_key': 'xpath_queue'
+            },
     },
-    CELERY_QUEUES=(
-        Queue('xpath_queue', exchange=Exchange('xpath_queue', type='direct'), routing_key='for_xpath')
-    ),
+
+    CELERY_QUEUES={
+        "xpath_queue": {
+            "exchange": "xpath_queue",
+            "exchange_type": "direct",
+            "routing_key": "xpath_queue"
+        }
+    }
 
 )
+
+
+
+
+
+
--- tasks/xpath.py
+++ tasks/xpath.py
@@ -11,14 +11,11 @@ def spider_task(main_url, xpath):
     xpathtexts.get_content(main_url,xpath)
 
 
-# There should be login interval, if too many accounts login at the same time from the same ip, all the
-# accounts can be banned by weibo's anti-cheating system
 @app.task(ignore_result=True)
 def excute_xpath_task():
     infos = xpath_info.get_xpath_info()
-    # Clear all stacked login tasks before each time for login
-    log.crawler.info('The login task is starting...')
+    log.crawler.info('The xpath task is starting...')
     for info in infos:
+        print(info.main_url)
         app.send_task('tasks.xpath.spider_task', args=(info.main_url, info.xpath), queue='xpath_queue',
                       routing_key='for_xpath')
-        time.sleep(10)
