ef0e6bde0c8d4629c3aa3cc5221efd7563c6250f
bsnail <2575652913@qq.com>
2019-08-13 15:05:13 +0800

    添加回调函数

--- ascio/aspider/request.py
+++ ascio/aspider/request.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+from inspect import iscoroutinefunction
 import chardet
 from ascio.aspider.utils import get_logger
 class Request():
@@ -16,12 +17,11 @@ class Request():
 
     METHOD = ['GET', 'POST']
 
-    def __init__(self, url, *,
-                 method: str = 'GET',
-                 request_config: dict = None,
-                 request_session=None,
+    def __init__(self, url:str,method: str = 'GET',*,
                  callback=None,
                  extra_value: dict = None,
+                 request_config: dict = None,
+                 request_session=None,
                  res_type: str = 'text',
                  **kwargs):
 
@@ -29,25 +29,32 @@ class Request():
         self.method = method.upper()
         if self.method not in self.METHOD:
             raise ValueError('%s method is not supported' % self.method)
+
+        self.callback = callback
+        self.extra_value = extra_value if extra_value is not None else {}
+        self.request_session = request_session
+
+
         if request_config is None:
             self.request_config = self.REQUEST_CONFIG
         else:
             self.request_config = request_config
-        self.request_session = request_session
-        self.callback = callback
-        self.extra_value = extra_value if extra_value is not None else {}
+
+
         self.res_type = res_type
         self.close_request_session = False
+        self.retry_times = self.request_config.get('RETRIES', 3)
         self.kwargs = kwargs
         self.logger = get_logger(name=self.name)
 
     @property
     def current_request_func(self):
         self.logger.info(f"<{self.method}: {self.url}>")
+        timeout = self.request_config.get('TIMEOUT', 10)
         if self.method == 'GET':
-            request_func = self.current_request_session.get(self.url, **self.kwargs)
+            request_func = self.current_request_session.get(self.url, timeout=timeout,**self.kwargs)
         else:
-            request_func = self.current_request_session.post(self.url, **self.kwargs)
+            request_func = self.current_request_session.post(self.url,timeout=timeout,**self.kwargs)
         return request_func
 
 
@@ -63,9 +70,9 @@ class Request():
     async def fetch(self):
         if self.request_config.get('DELAY', 0) > 0:
             await asyncio.sleep(self.request_config['DELAY'])
-
-        async with self.current_request_func as resp:
-            if resp.status in [200, 201]:
+        try:
+            async with self.current_request_func as resp:
+                assert resp.status in [200, 201]
                 if self.res_type == 'bytes':
                     data = await resp.read()
                 elif self.res_type == 'json':
@@ -74,15 +81,33 @@ class Request():
                     content = await resp.read()
                     charset = chardet.detect(content)
                     data = content.decode(charset['encoding'])
-            else:
-                self.logger.error(f"<Error: {self.url} {resp.status}>")
-                data = None
+
+        except Exception:
+            self.logger.error(f"<Error: {self.url} {resp.status}>")
+            data = None
+
+        if self.retry_times > 0 and data is None:
+            retry_times = self.request_config.get('RETRIES', 3) - self.retry_times + 1
+            self.logger.info(f'<Retry url: {self.url}>, Retry times: {retry_times}')
+            self.retry_times -= 1
+            return await self.fetch()
 
         if self.close_request_session:
             await self.request_session.close()
 
         return type('Response', (),
                     {'html': data, 'url': self.url, 'extra_value': self.extra_value})
+
+    async def fetch_callback(self):
+        res = await self.fetch()
+        if self.callback is not None:
+            if iscoroutinefunction(self.callback):
+                res = await self.callback(res)
+            else:
+                res = self.callback(res)
+        return res
+
+
 
     def __str__(self):
         return "<%s %s>" % (self.method, self.url)
